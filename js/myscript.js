23c282aa90c73ff1953a5a0aee426c559d0fec2f
Brian Marinai <brianmarinai.bm@gmail.com>
2020-06-25 16:47:33 +0200

    esercizio terminato

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,17 @@
+// scrivi un programma che stampi i numeri da 1 a 100,
+// per i multipli di 3 stampi "Fizz"
+// per i multipli di 5 stampi "Buzz"
+// per i multipli di 3 e 5 stampi "FizzBuzz"
 
-
-var numeri = [];
 
 for( var i = 1; i < 101; i++){
-  var numbers = i;
-  numeri.push(i);
-  if (i % 3 == 0) {
-    console.log("multiplo di 3");
+  if (i % 15 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
 }
